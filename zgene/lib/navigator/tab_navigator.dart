c6280f6d3e63f6912728fca339cccd5cd610d1ab
chenchenchen <1715666965@qq.com>
2021-08-19 14:55:54 +0800

    首页导航

---
+++ zgene/lib/navigator/tab_navigator.dart
@@ -0,0 +1,78 @@
+/**
+ * 《Flutter从入门到进阶-实战携程网App》
+ * 课程地址：
+ * https://coding.imooc.com/class/321.html
+ * 课程代码、文档：
+ * https://git.imooc.com/coding-321/
+ * 课程辅导答疑区：
+ * http://coding.imooc.com/learn/qa/321.html
+ */
+import 'package:flutter/material.dart';
+import 'package:zgene/pages/buy_page.dart';
+import 'package:zgene/pages/home_page.dart';
+import 'package:zgene/pages/my_page.dart';
+import 'package:zgene/pages/report_page.dart';
+
+class TabNavigator extends StatefulWidget {
+  @override
+  _TabNavigatorState createState() => _TabNavigatorState();
+}
+
+class _TabNavigatorState extends State<TabNavigator> {
+  final _defaultColor = Colors.grey;
+  final _activeColor = Colors.blue;
+  int _currentIndex = 0;
+  final PageController _controller = PageController(
+    initialPage: 0,
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PageView(
+        controller: _controller,
+        children: <Widget>[
+          HomePage(),
+          ReportPage(),
+          BuyPage(),
+          MyPage(),
+        ],
+        physics: NeverScrollableScrollPhysics(),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _currentIndex,
+          onTap: (index) {
+            _controller.jumpToPage(index);
+            setState(() {
+              _currentIndex = index;
+            });
+          },
+          type: BottomNavigationBarType.fixed,
+          items: [
+            _bottomItem('首页', Icons.home, 0),
+            _bottomItem('报告', Icons.report, 1),
+            _bottomItem('购买', Icons.reorder, 2),
+            _bottomItem('我的', Icons.account_circle, 3),
+          ]),
+    );
+  }
+
+  _bottomItem(String title, IconData icon, int index) {
+    return BottomNavigationBarItem(
+        icon: Icon(
+          icon,
+          color: _defaultColor,
+        ),
+        activeIcon: Icon(
+          icon,
+          color: _activeColor,
+        ),
+        label: title,
+        // title: Text(
+        //   title,
+        //   style: TextStyle(
+        //       color: _currentIndex != index ? _defaultColor : _activeColor),
+        // )
+    );
+  }
+}
---
+++ zgene/lib/pages/buy_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class BuyPage extends StatefulWidget {
+  @override
+  _BuyPageState createState() => _BuyPageState();
+}
+
+
+class _BuyPageState extends State<BuyPage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text("购买"),
+    );
+  }
+}

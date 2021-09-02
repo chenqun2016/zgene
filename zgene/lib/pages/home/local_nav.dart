f4ddb1ae4408b0304b3b7dda570bc0accbb800d2
sunshanglei <1687037382@qq.com>
2021-09-02 14:49:26 +0800

    修改各种种类

--- zgene/lib/pages/home/local_nav.dart
+++ zgene/lib/pages/home/local_nav.dart
@@ -1,9 +1,6 @@
-import 'dart:collection';
 
 import 'package:flutter/material.dart';
-import 'package:zgene/constant/api_constant.dart';
 import 'package:zgene/constant/color_constant.dart';
-import 'package:zgene/http/http_utils.dart';
 import 'package:zgene/models/content_model.dart';
 import 'package:zgene/navigator/navigator_util.dart';
 import 'package:zgene/pages/bindcollector/bind_collector_page.dart';
@@ -11,7 +8,7 @@ import 'package:zgene/pages/my/add_address_page.dart';
 import 'package:zgene/pages/my/order_step_page.dart';
 import 'package:zgene/util/common_utils.dart';
 import 'package:zgene/util/time_utils.dart';
-import 'package:zgene/util/ui_uitls.dart';
+import 'home_getHttp.dart';
 
 class LocalNav extends StatefulWidget {
   @override
@@ -24,7 +21,13 @@ class _LocalNavState extends State<LocalNav> {
   @override
   void initState() {
     super.initState();
-    getHttp(9);
+    HomeGetHttp(9, (result) {
+      ContentModel contentModel = ContentModel.fromJson(result);
+      goldList.clear();
+      setState(() {
+        goldList=contentModel.archives;
+      });
+    });
   }
 
   @override
@@ -95,29 +98,4 @@ class _LocalNavState extends State<LocalNav> {
     );
   }
 
-  ///获取内容列表
-  getHttp(type) async {
-    bool isNetWorkAvailable = await CommonUtils.isNetWorkAvailable();
-    if (!isNetWorkAvailable) {
-      return;
-    }
-    Map<String, dynamic> map = new HashMap();
-    map['cid'] =
-        type; //栏目ID 9:金刚区 10:Banner 11:探索之旅 12:独一无二的你 3:常见问题 6:示例报告（男） 7:示例报告（女） 15：精选报告
-    HttpUtils.requestHttp(
-      ApiConstant.contentList,
-      parameters: map,
-      method: HttpUtils.GET,
-      onSuccess: (result) async {
-        ContentModel contentModel = ContentModel.fromJson(result);
-        goldList.clear();
-        setState(() {
-          goldList = contentModel.archives;
-        });
-      },
-      onError: (code, error) {
-        UiUitls.showToast(error);
-      },
-    );
-  }
 }

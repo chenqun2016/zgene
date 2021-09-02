63831f99e06ede9dc2c96a83482a86a981158a83
chenchenchen <1715666965@qq.com>
2021-09-02 19:34:16 +0800

    报告列表

--- zgene/lib/pages/report/report_list_page.dart
+++ zgene/lib/pages/report/report_list_page.dart
@@ -4,6 +4,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:zgene/constant/color_constant.dart';
 import 'package:zgene/util/base_widget.dart';
+import 'package:zgene/util/ui_uitls.dart';
 
 class ReportListPage extends BaseWidget {
   @override
@@ -11,45 +12,26 @@ class ReportListPage extends BaseWidget {
 }
 
 class _ReportListPageState extends BaseWidgetState<ReportListPage> {
-  final data = <Color>[
-    Colors.purple[50],
-    Colors.purple[100],
-    Colors.purple[200],
-    Colors.purple[300],
-    Colors.purple[400],
-    Colors.purple[500],
-    Colors.purple[600],
-    Colors.purple[700],
-    Colors.purple[800],
-    Colors.purple[900],
-    Colors.purple[50],
-    Colors.purple[100],
-    Colors.purple[200],
-    Colors.purple[300],
-    Colors.purple[400],
-    Colors.purple[500],
-    Colors.purple[600],
-    Colors.purple[700],
-    Colors.purple[800],
-    Colors.purple[900],
-  ];
+  final data = <int>[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
 
   @override
   void pageWidgetInitState() {
     super.pageWidgetInitState();
 
     showBaseHead = false;
-    showHead = false;
-    // pageWidgetTitle = "运动健康";
+    showHead = true;
+    pageWidgetTitle = "运动健康";
     isListPage = true;
-    backColor = Colors.red;
-    // backImgPath = "assets/images/home/bg_home.png";
+    // backColor = Colors.red;
+    backImgPath = "assets/images/mine/img_bg_my.png";
   }
 
   Widget viewPageBody(BuildContext context) {
     return Stack(
       children: [
         CustomScrollView(
+          physics: BouncingScrollPhysics(),
+          controller: listeningController,
           slivers: <Widget>[
             _buildSliverAppBar(),
             _buildPersistentHeader(),
@@ -97,15 +79,28 @@ class _ReportListPageState extends BaseWidgetState<ReportListPage> {
   Widget _buildPersistentHeader() => SliverPersistentHeader(
       pinned: true,
       delegate: _SliverDelegate(
-        minHeight: 44.0,
-        maxHeight: 44.0,
+        minHeight: 50.0,
+        maxHeight: 50.0,
         child: Container(
-            color: Colors.white.withOpacity(0.6),
+            height: 50,
+            decoration: BoxDecoration(
+              color: Color.fromARGB(trans, 255, 255, 255),
+              // color: Colors.white,
+              border: Border.all(
+                color: Colors.white,
+                width: 1,
+              ),
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(25 * (1 - trans / 255)),
+                topRight: Radius.circular(25 * (1 - trans / 255)),
+              ),
+            ),
             child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 Padding(
-                  padding: EdgeInsets.only(left: 64),
+                  padding: EdgeInsets.only(left: 64, top: 14),
                   child: Text(
                     "项目",
                     style: TextStyle(
@@ -115,7 +110,7 @@ class _ReportListPageState extends BaseWidgetState<ReportListPage> {
                   ),
                 ),
                 Padding(
-                  padding: EdgeInsets.only(right: 64),
+                  padding: EdgeInsets.only(right: 64, top: 14),
                   child: Text(
                     "结果",
                     style: TextStyle(
@@ -132,16 +127,16 @@ class _ReportListPageState extends BaseWidgetState<ReportListPage> {
         child: Container(
           decoration: BoxDecoration(
             color: Colors.white,
-            borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(20),
-              topRight: Radius.circular(20),
-            ),
+            // borderRadius: BorderRadius.only(
+            //   topLeft: Radius.circular(20),
+            //   topRight: Radius.circular(20),
+            // ),
           ),
           child: ListView.builder(
             shrinkWrap: true,
             itemCount: data.length,
             physics: BouncingScrollPhysics(),
-            padding: EdgeInsets.fromLTRB(0, 12, 0, 100),
+            padding: EdgeInsets.fromLTRB(0, 12, 0, 140),
             itemBuilder: (BuildContext context, int index) {
               return _buildSliverItem(index);
             },
@@ -150,97 +145,206 @@ class _ReportListPageState extends BaseWidgetState<ReportListPage> {
       );
 
   Widget _buildSliverItem(index) {
-    return Opacity(
-      opacity: index < 3 ? 1 : 0.4,
-      child: Padding(
-        padding: EdgeInsets.only(left: 16, right: 16, top: 12, bottom: 13),
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Expanded(
-              child: Text(
-                "锻炼对体重指数效果",
-                style: TextStyle(
-                    color: ColorConstant.TextMainBlack,
-                    fontWeight: FontWeight.w500,
-                    fontSize: 15),
+    return GestureDetector(
+      onTap: () {
+        UiUitls.showToast("index==$index");
+      },
+      child: Opacity(
+        opacity: index < 3 ? 1 : 0.4,
+        child: Padding(
+          padding: EdgeInsets.only(left: 16, right: 16, top: 12, bottom: 13),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Expanded(
+                child: Text(
+                  "锻炼对体重指数效果",
+                  style: TextStyle(
+                      color: ColorConstant.TextMainBlack,
+                      fontWeight: FontWeight.w500,
+                      fontSize: 15),
+                ),
               ),
-            ),
-            Image.asset(
-              "assets/images/report/img_qiang.png",
-              width: 22,
-              height: 22,
-            ),
-            Padding(
-              padding: EdgeInsets.only(left: 6, right: 28),
-              child: Text(
-                "弱",
-                style: TextStyle(
-                    color: ColorConstant.Text_8E9AB,
-                    fontWeight: FontWeight.w400,
-                    fontSize: 16),
+              Image.asset(
+                "assets/images/report/img_qiang.png",
+                width: 22,
+                height: 22,
               ),
-            ),
-            Image.asset(
-              "assets/images/mine/icon_my_name_right.png",
-              width: 22,
-              height: 22,
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-
-  Widget _buildSliverAppBar() {
-    return SliverAppBar(
-      expandedHeight: 277.0,
-      leading: _buildLeading(),
-      title: Text(
-        '运动健康',
-        style: TextStyle(
-            fontSize: 18,
-            color: ColorConstant.TextMainBlack,
-            fontWeight: FontWeight.bold),
-      ),
-      centerTitle: true,
-      elevation: 0,
-      foregroundColor: Colors.transparent,
-      pinned: true,
-      backgroundColor: Colors.white,
-      flexibleSpace: FlexibleSpaceBar(
-        //伸展处布局
-        titlePadding: EdgeInsets.only(left: 55, bottom: 15),
-        //标题边距
-        collapseMode: CollapseMode.parallax,
-        //视差效果
-        background: Container(
-          color: Colors.blue,
-          padding: EdgeInsets.fromLTRB(16, 109, 16, 0),
-          child: Image.asset(
-            "assets/images/report/banner_yundong.png",
-            fit: BoxFit.fill,
+              Padding(
+                padding: EdgeInsets.only(left: 6, right: 28),
+                child: Text(
+                  "弱",
+                  style: TextStyle(
+                      color: ColorConstant.Text_8E9AB,
+                      fontWeight: FontWeight.w400,
+                      fontSize: 16),
+                ),
+              ),
+              Image.asset(
+                "assets/images/mine/icon_my_name_right.png",
+                width: 22,
+                height: 22,
+              ),
+            ],
           ),
         ),
       ),
     );
   }
 
-  Widget _buildLeading() => GestureDetector(
-        onTap: () {
-          Navigator.pop(context);
-        },
-        child: Container(
-            margin: EdgeInsets.all(10),
-            child: Image.asset(
-              'assets/images/icon_base_backArrow.png',
-              height: 40,
-              width: 40,
-            )),
-      );
+  Widget _buildSliverAppBar() {
+    return SliverToBoxAdapter(
+      child: Stack(
+        children: [
+          Container(
+            width: double.infinity,
+            height: 148,
+            // decoration: BoxDecoration(
+            //     image: DecorationImage(
+            //   image: AssetImage("assets/images/report/banner_yundong.png"),
+            //   fit: BoxFit.fill,
+            // )),
+            margin: EdgeInsets.fromLTRB(20, 20, 20, 0),
+            child: Stack(
+              children: [
+                ClipRect(
+                  child: Image.asset(
+                    "assets/images/report/banner_yundong.png",
+                    fit: BoxFit.cover,
+                    alignment: Alignment.topCenter,
+                    width: double.infinity,
+                    height: 148,
+                  ),
+                ),
+                _titleContent,
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 
-  String colorString(Color color) =>
-      "#${color.value.toRadixString(16).padLeft(8, '0').toUpperCase()}";
+  Widget get _titleContent {
+    return Padding(
+      padding: EdgeInsets.fromLTRB(30, 22, 0, 0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            "运动健身",
+            style: TextStyle(
+                fontWeight: FontWeight.bold, color: Colors.white, fontSize: 28),
+          ),
+          Text(
+            "共12项",
+            style: TextStyle(
+                fontWeight: FontWeight.w500, color: Colors.white, fontSize: 14),
+          ),
+          Row(
+            children: [
+              Container(
+                margin: EdgeInsets.only(top: 16, right: 10),
+                padding: EdgeInsets.fromLTRB(8, 4, 8, 4),
+                decoration: BoxDecoration(
+                  color: Colors.white24,
+                  borderRadius: BorderRadius.all(Radius.circular(32)),
+                ),
+                child: Row(
+                  children: [
+                    Container(
+                      height: 12,
+                      width: 12,
+                      margin: EdgeInsets.only(right: 6),
+                      decoration: BoxDecoration(
+                        gradient: LinearGradient(colors: [
+                          Color(0xFF47FEDB),
+                          Color(0xFF23CFAF),
+                        ]),
+                        shape: BoxShape.circle,
+                        border: Border.all(
+                          color: Colors.white,
+                          width: 1,
+                        ),
+                      ),
+                    ),
+                    Text(
+                      "强  04",
+                      style: TextStyle(color: Colors.white, fontSize: 12),
+                    )
+                  ],
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(top: 16, right: 10),
+                padding: EdgeInsets.fromLTRB(8, 4, 8, 4),
+                decoration: BoxDecoration(
+                  color: Colors.white24,
+                  borderRadius: BorderRadius.all(Radius.circular(32)),
+                ),
+                child: Row(
+                  children: [
+                    Container(
+                      height: 12,
+                      width: 12,
+                      margin: EdgeInsets.only(right: 6),
+                      decoration: BoxDecoration(
+                        gradient: LinearGradient(colors: [
+                          Color(0xFF5EECFD),
+                          Color(0xFF248DFA),
+                        ]),
+                        shape: BoxShape.circle,
+                        border: Border.all(
+                          color: Colors.white,
+                          width: 1,
+                        ),
+                      ),
+                    ),
+                    Text(
+                      "中  07",
+                      style: TextStyle(color: Colors.white, fontSize: 12),
+                    )
+                  ],
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(top: 16, right: 10),
+                padding: EdgeInsets.fromLTRB(8, 4, 8, 4),
+                decoration: BoxDecoration(
+                  color: Colors.white24,
+                  borderRadius: BorderRadius.all(Radius.circular(32)),
+                ),
+                child: Row(
+                  children: [
+                    Container(
+                      height: 12,
+                      width: 12,
+                      margin: EdgeInsets.only(right: 6),
+                      decoration: BoxDecoration(
+                        gradient: LinearGradient(colors: [
+                          Color(0xFFFE8B8C),
+                          Color(0xFFFE4343),
+                        ]),
+                        shape: BoxShape.circle,
+                        border: Border.all(
+                          color: Colors.white,
+                          width: 1,
+                        ),
+                      ),
+                    ),
+                    Text(
+                      "弱  01",
+                      style: TextStyle(color: Colors.white, fontSize: 12),
+                    )
+                  ],
+                ),
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
 }
 
 class _SliverDelegate extends SliverPersistentHeaderDelegate {

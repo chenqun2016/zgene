74890c8adacc13ad90b6e580ce35396bdc3bfde9
tianya <tianya@benshar.com>
2021-09-13 16:38:38 +0800

    webapp

--- zgene/lib/configure_web.dart
+++ zgene/lib/configure_web.dart
@@ -10,10 +10,10 @@ void configureApp() {
 void webLogin() {
   // 如果是web则需要进行一些js操作
   var res = js.context.callMethod('GetCookie', ['jwt']);
-  if (res != null && !res) {
+  // print("xxxxxrs = ${res}");
+  if (res != null) {
     var spUtils = SpUtils();
     spUtils.setStorage(SpConstant.Token, res);
     spUtils.setStorage(SpConstant.IsLogin, true);
-    // js.context.callMethod('alert', [res]);
   }
 }

3fe8dd89bb98b8851917f21900c217e88a92a395
warhol <494605779@qq.com>
2021-10-14 13:01:36 +0800

    安卓添加配置

--- zgene/android/app/src/main/kotlin/com/jt/zgene/MainActivity.kt
+++ zgene/android/app/src/main/kotlin/com/jt/zgene/MainActivity.kt
@@ -1,6 +1,19 @@
 package com.jt.zgene
 
+import android.content.Intent 
+import android.os.Bundle
+import android.util.Log
+
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //获取自定义透传参数值
+            val url  = intent.getStringExtra("url")
+            val id   = intent.getStringExtra("id")
+            Log.d("--------------", "url = " + url + ",id = " + id)
+        
+    }
+     
 }
--- zgene/lib/util/getui_Utils.dart
+++ zgene/lib/util/getui_Utils.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 import 'dart:ffi';
+import 'dart:io';
 
 import 'package:getuiflut/getuiflut.dart';
 import 'package:zgene/constant/common_constant.dart';
@@ -17,32 +18,29 @@ class GetuiUtils {
       },
       onReceiveMessageData: (Map<String, dynamic> msg) async {
         print("flutter onReceiveMessageData: $msg");
-        var payload = json.decode(msg["payload"].toString());
         print("------------------");
+        var payload = json.decode(msg["payload"].toString());
         print(payload);
 
         CommonUtils.globalToUrl(type: payload["type"], url: payload["url"]);
       },
       onNotificationMessageArrived: (Map<String, dynamic> msg) async {
         print("flutter onNotificationMessageArrived");
-        print("++++++++++++++++");
       },
       onNotificationMessageClicked: (Map<String, dynamic> msg) async {
-        print("flutter onNotificationMessageClicked");
+        print("flutter onNotificationMessageClicked: $msg");
+
         print("++++++++++++++++");
       },
       onRegisterDeviceToken: (String message) async {},
       onReceivePayload: (Map<String, dynamic> message) async {
         print("flutter onReceivePayload: $message");
-        print("++++++++++++++++");
       },
       onReceiveNotificationResponse: (Map<String, dynamic> msg) async {
         print("flutter onReceiveNotificationResponse : $msg");
-        var payload = json.decode(msg["payload"].toString());
-        print("------------------");
-        print(payload);
-        CommonUtils.globalToUrl(type: payload["type"], url: payload["url"]);
-        print("888888888888888");
+        if (Platform.isIOS) {
+          CommonUtils.globalToUrl(type: msg["type"], url: msg["url"]);
+        }
       },
       onAppLinkPayload: (String message) async {},
       onRegisterVoipToken: (String message) async {},
